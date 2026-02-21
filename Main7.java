7a5f2e40c0afc4079e85ee93f50eafb18c698829
AdithiKulal <kulalamithadinakar@gmail.com>
2026-02-21 18:23:02 +0530

    Add files via upload

---
+++ Main7.java
@@ -0,0 +1,11 @@
+public class Main7 {
+    int age;
+    Main7(int age) {
+        this.age = age;
+    }
+
+public static void main(String[] args) {
+    Main7 obj = new Main7(24);
+    System.out.println("obj.age = " + obj.age);
+   }
+}
